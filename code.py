0f467a0207440c45e614bd898aa261023d3067d2
Yash Jha <r22133yash@dpsrkp.net>
2021-07-24 01:17:14 +0530

    Update Brute-force search space

--- code.py
+++ code.py
@@ -8,6 +8,11 @@ A = [[1,1,0,1,0],
 	 [0,-1,-1,1,2],
 	 [1,0,0,0,0],
 	 [0,0,0,0,1]]
+A = [[1,1,0,1,0],
+	 [0,-1,1,1,1],
+	 [0,-1,-1,1,2],
+	 [0,0,0,-1,1],
+	 [0,0,0,0,1]]
 # B = [[1],[1],[1]]
 def is_decimal(tensor):
 	if(torch.all(tensor.long()==tensor)):
@@ -16,18 +21,26 @@ def is_decimal(tensor):
 		return True
 
 C = [[1],[1],[1],[1],[1]]
-
+def getRanges(row):
+	global A
+	return range(1,torch.sum(A[row]!=0)+1)
 A = torch.Tensor(A)
 # B = torch.Tensor(B)
 C = torch.Tensor(C)
 
 vals = range(1,3)
-
-for a in vals:
-	for b in vals:
-		for c in vals:
-			for d in vals:
-				for e in vals:
+print(getRanges(0))
+print(getRanges(1))
+print(getRanges(2))
+print(getRanges(3))
+print(getRanges(4))
+# quit()
+for a in getRanges(0):
+	for b in getRanges(1):
+		for c in getRanges(2):
+			for d in getRanges(3):
+				for e in getRanges(4):
+					print(a,b,c,d,e)
 					C = [[a],[b],[c],[d],[e]]
 					C = torch.Tensor(C)
 					# print(torch.mm(B,A))
