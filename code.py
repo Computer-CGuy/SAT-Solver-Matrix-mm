2bacdd3d3826ac49b36e3451f2f3bdb6af946abe
Yash Jha <r22133yash@dpsrkp.net>
2021-07-26 01:57:58 +0530

    Made automatic answer possible

--- code.py
+++ code.py
@@ -16,7 +16,7 @@ A = [[1,1,0,1,0],
 	 [0,0,0,-1,1],
 	 [0,0,0,0,1]]
 A = [[1,0,0,0],
-	 [1,1,0,0],
+	 [0,1,0,0],
 	 [0,0,1,0],
 	 [1,1,1,1]]
 # B = [[1],[1],[1]]
@@ -35,36 +35,52 @@ def getRanges(row):
 # B = torch.Tensor(B)
 C = torch.Tensor(C)
 
-# vals = range(1,3)
-# print(getRanges(0))
-# print(getRanges(1))
-# print(getRanges(2))
-# print(getRanges(3))
-# print(getRanges(4))
-# quit()
 def calcVal(mat):
 	cnt = 0
 	for x in mat:
 		if(x==0.0 or x==1.0):
 			continue
+
+		# cnt+=(x+1-x)/2
 		cnt+=max(0-x,1-x)
 	return int(cnt)
 def DiffRet(a,b):
 	return torch.sum((b-a))
-def printMe(a,b,c,d):
+def printMe(vals,ret_=False):
+	a,b,c,d = vals
 	C = [[a],[b],[c],[d]]
 	C = torch.Tensor(C)
+	# print(C)
 	# print(torch.mm(B,A))
 	# print(torch.inverse(A))
 	ret = (torch.mm(torch.inverse(A),C))
+	if(ret_):
+		print(vals)
+		print(ret)
 	# if(not is_decimal(ret)):
-	print(a,b,c,d)
-	print(ret)
-	print("Val",calcVal(ret))
-	return ret
+	# print(a,b,c,d)
+	# print(ret)
+	# print("Val",calcVal(ret))
+	return calcVal(ret)
 	# print(list(x for x in ret))
 	# print(is_decimal(ret))
-ret2 = printMe(1,1,1,1)
-ret3 = printMe(1,2,1,1)
-ret3 = printMe(1,3,1,1)
-print(DiffRet(ret2,ret3))
+vals = [1]*4
+print(vals)
+mina = printMe(vals)
+for i in range(4):
+	vals[i]+=1
+	if(printMe(vals,ret_=False)>=mina):
+		vals[i]-=1
+mina = printMe(vals)
+for i in range(4):
+	vals[i]+=1
+	if(printMe(vals,ret_=False)>=mina):
+		vals[i]-=1
+print(vals)
+printMe(vals,ret_=True)
+# print(printMe([1,1,1,1],ret_=True))
+# print(printMe([1,1,2,1],ret_=True))
+# ret2 = printMe(1,1,2,1,ret_=True)
+# ret3 = printMe(1,2,1,1)
+# ret3 = printMe(1,3,1,1)
+# print(DiffRet(ret2,ret3))
