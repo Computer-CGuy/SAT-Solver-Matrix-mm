cb65fe9d5a0c716cb21275704633484facf00300
Yash Jha <r22133yash@dpsrkp.net>
2021-07-26 17:48:26 +0530

    Fixed nVariables and nClauses bug

--- utils.py
+++ utils.py
@@ -1,9 +1,15 @@
 import torch
+# torch.set_printoptions(precision=3)
 def listToInt(vals):
     return list(map(int,vals))
 def sgn(i):
     return (-1 if i<0 else 1)
+def safePrint(matrix):
+    matrix[matrix<0.001]=0
+    print(matrix)
+num_clauses = 0
 def read_cnf_from_file(file_name):
+    global num_clauses
     f = open(file_name,"r")
     # line = (line.strip())
     clause_id = 0
@@ -13,10 +19,11 @@ def read_cnf_from_file(file_name):
         if line[0]=="p":
             variables, clauses = line.replace("p cnf ","").split(" ")
             variables, clauses = int(variables),int(clauses)
+            num_clauses = clauses
             # shape = max(variables,clauses)
-            shape = max(variables,clauses)
-            shape+=1
-            A = torch.zeros((variables+1,clauses))
+            
+            # A = torch.zeros((variables+2,clauses))
+            A = torch.zeros((clauses+1,variables+1))
             continue
         vals = listToInt(line.split(" "))[:-1]
         negs = 0
@@ -28,6 +35,7 @@ def read_cnf_from_file(file_name):
             try:
                 A[clause_id][x-1]= sgn_
             except IndexError:
+                print("Index Error")
                 A[clause_id-1][-1] = negs
                 A[-1][-1] = 1
                 return A
@@ -38,30 +46,16 @@ def read_cnf_from_file(file_name):
         
 
 # ret = read_cnf_from_file("aim-50-1_6-yes1-4.cnf")
-# ret = read_cnf_from_file("simple_v3_c2.cnf")
-ret = read_cnf_from_file("simple.cnf")
+ret = read_cnf_from_file("simple_v3_c2.cnf")
+# ret = read_cnf_from_file("simple.cnf")
+# ret = read_cnf_from_file("simple.cnf")
 # print(ret)
 
-# ret = \
-# torch.Tensor([[1., 1., 0., 0., 0., 0., 0., 0., 0.],
-#         [0., 0., 1., 1., 0., 0., 0., 0., 0.],
-#         [1., 1., 0., 0., 0., 0., 0., 0., 0.],
-#         [1., 1., 0., 0., 0., 0., 0., 0., 0.],
-#         [0., 0., 1., 0., 1., 0., 0., 1., 0.],
-#         [0., 1., 1., 0., 0., 0., 0., 1., 0.],
-#         [0., 0., 0., 1., 1., 0., 0., 1., 0.],
-#         [0., 0., 0., 0., 1., 1., 1., 1., 0.],
-#         [0., 0., 0., 0., 0., 0., 0., 0., 1.]])
-# # ret[-2,-3:]=1
-# ret[6,-1]=1
-# print(ret)
-# 1 2 3 0 
-# 3 4 5 0
-# 1 2 4 0
-# 1 2 5 0
-# 3 5 6 0
-# 2 3 4 0
-# 4 5 6 0
 C = 7*[[3]]+[[1]]
+
 C = [[2],[2],[1],[2],[2],[1],[1],[1]]
-print(torch.mm(torch.pinverse(ret),torch.Tensor(C)))
+C = (num_clauses)*[[1]]+[[1]]
+print(ret)
+ans = torch.mm(torch.pinverse(ret),torch.Tensor(C))
+safePrint(ans)
+# print()
